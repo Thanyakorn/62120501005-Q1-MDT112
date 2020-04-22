0383624ffc996400391fe0b595db459eca4f29d1
thanyakorn.nak <thanyakorn.nak@mail.kmutt.ac.th>
2020-04-22 19:58:49 +0700

    commit to 03

--- 62120501005-Q1-MDT112/62120501005-Q1-MDT112.ino
+++ 62120501005-Q1-MDT112/62120501005-Q1-MDT112.ino
@@ -1,107 +1,80 @@
-#define buzzer 8
 void setup(){
-
-    Serial.begin(9600);
+    Serial.begin(9600);   
     pinMode(2,INPUT_PULLUP);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);   
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
-    
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);  
+    pinMode(9,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(13,OUTPUT);  
+
+
+    tone(8,200,500);            
+    delay(250);                
+    tone(8,400,100);
+
 }
+
+  int L = 3;  int k = 0;  int s = 100;
+
 void loop(){
-    
+
+    for (L = 3; L <= 13; L++)
+    {
+        if(L == 8)
+        {
+            digitalWrite(L,LOW);
+            continue;
+        }
+
+        digitalWrite(L,HIGH);
+        while (digitalRead(2) == 0)
+
+        {   
+            delay(100);
+            if (digitalRead(2) == 1)
+            {               
+                k++;
+                tone(8,400,100);        
+                Serial.println("Faster X 2 Current Speed : X" + String(pow(2,k)));                       
+                s = s/2;
+            }
+        }
+
+        delay(s);
+        digitalWrite(L,LOW);       
+    }
 
 
-    digitalWrite(2,HIGH);  
-    delay(100);  
-    digitalWrite(2,LOW);
 
-    digitalWrite(3,HIGH);  
-    delay(100);  
-    digitalWrite(3,LOW);
+    for (L = 13; L >= 3; L--)
 
-    digitalWrite(4,HIGH);
-    delay(100);
-    digitalWrite(4,LOW);
+    {
+        if(L == 8)
+        {
+            digitalWrite(L,LOW);
+            continue;
+        }
 
-    digitalWrite(5,HIGH);
-    delay(100);
-    digitalWrite(5,LOW);
+        digitalWrite(L,HIGH);
+        while (digitalRead(2) == 0)
 
-    digitalWrite(6,HIGH);   
-    delay(100); 
-    digitalWrite(6,LOW);
+        {   
+            delay(100);
+            if (digitalRead(2) == 1)
+            {               
+                k++;
+                tone(8,400,100);        
+                Serial.println("Faster X 2 Current Speed : X" + String(pow(2,k)));                             
+                s = s/2;
+            }
+        }     
 
-    digitalWrite(7,HIGH);
-    delay(100);
-    digitalWrite(7,LOW);
-   
-    digitalWrite(9,HIGH);   
-    delay(100);
-    digitalWrite(9,LOW);
-
-    digitalWrite(10,HIGH);
-    delay(100);
-    digitalWrite(10,LOW);
-
-    digitalWrite(11,HIGH);
-    delay(100);
-    digitalWrite(11,LOW);
-
-    digitalWrite(12,HIGH);
-    delay(100);   
-    digitalWrite(12,LOW);
-
-    digitalWrite(13,HIGH);
-    delay(100);
-    digitalWrite(13,LOW);
-
-
-    digitalWrite(12,HIGH);  
-    delay(100);  
-    digitalWrite(12,LOW);
-
-    digitalWrite(11,HIGH);
-    delay(100);
-    digitalWrite(11,LOW);
-
-    digitalWrite(10,HIGH);
-    delay(100);
-    digitalWrite(10,LOW);
-
-    digitalWrite(9,HIGH);   
-    delay(100); 
-    digitalWrite(9,LOW);
-
-    digitalWrite(7,HIGH);
-    delay(100);
-    digitalWrite(7,LOW);
-
-    digitalWrite(6,HIGH);   
-    delay(100);
-    digitalWrite(6,LOW);
-
-    digitalWrite(5,HIGH);
-    delay(100);
-    digitalWrite(5,LOW);
-
-    digitalWrite(4,HIGH);
-    delay(100);
-    digitalWrite(4,LOW);
-
-    digitalWrite(3,HIGH);
-    delay(100);   
-    digitalWrite(3,LOW);
-
-    digitalWrite(2,HIGH);  
-    delay(100);  
-    digitalWrite(2,LOW);
-    
+        delay(s);
+        digitalWrite(L,LOW);         
+    }
 }
